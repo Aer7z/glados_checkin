7d290b04079d341bac2358dea703441276f27d9d
Aer7z <141202918+Aer7z@users.noreply.github.com>
2024-03-31 20:54:42 +0800

    Update main.js
    尝试修复bug

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
-const glados = async () => {
-  const cookie = process.env.GLADOS
-  if (!cookie) return
-  try {
-    const headers = {
-      'cookie': cookie,
-      'referer': 'https://glados.rocks/console/checkin',
-      'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+
+const gladosRequestPush=async()=>{
+  const cookie = process.env.GLADOS_COOKIE;//基于cookie的验证
+  const authorization = process.env.GLADOS_AUTHORIZATION;//基于authorization的令牌验证
+  // Node.js环境中，process是一个全局对象，它提供了有关当前Node.js进程的信息和控制权。process.env是process的属性，它是一个包含了系统环境变量的对象
+  if(!cookie)return ;
+  //未设置cookie无法登录则返回,cookie用于标记是用户信息，同一用户在同一网站中的行为产生的网络请求中的cookie都是一致的。
+let checkin;
+let status;
+  try{
+    const headers={//请求头不区分大小写
+      cookie,//维持会话状态
+      authorization,//验证权限
+      //下面是给服务器识别为 真人 使用
+      'referrer':'https://glados.rocks/console/checkin',//网络请求发出的那个地方的网址
+      'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
     }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
-      method: 'POST',
-      headers: { ...headers, 'content-type': 'application/json' },
-      body: '{"token":"glados.one"}',
-    }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
-      method: 'GET',
+    const checkinRequestURL='https://glados.rocks/api/user/checkin'
+    checkin=await fetch(checkinRequestURL,{
+      method:'POST',
+      headers:{...headers,  'content-type':'application/json;charset=UTF-8'},
+      //只有POST方法需要content-type
+      body:'{"token":"glados.one"}'
+    }).then((r)=>r.json());
+    const statusRequestURL ='https://glados.rocks/api/user/status'
+    status=await fetch(statusRequestURL,{
+      method:'GET',
       headers,
-    }).then((r) => r.json())
+    }).then((r)=>r.json());
     return [
-      'Checkin OK',
-      `${checkin.message}`,
-      `Left Days ${Number(status.data)}`,
+      'GLADOS 签到成功',
+      `checkin 信息:${checkin}`,
+      `status 信息:${status}`
     ]
-  } catch (error) {
+  }catch(error){
     return [
-      'Checkin Error',
-      `${error}`,
-      `<${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}>`,
-    ]
+      'GLADOS 签到失败',
+      `checkin 信息:${checkin}`,
+      `status 信息:${status}`
+      ]
   }
 }
 
@@ -45,8 +56,8 @@ const notify = async (contents) => {
   })
 }
 
-const main = async () => {
-  await notify(await glados())
+const main = async()=>{
+  await notify(await gladosRequestPush())
 }
 
 main()
