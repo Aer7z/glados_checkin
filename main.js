a04c4b03f7e675fba4543e328fe9956356a1e955
Aer7z <570991904@qq.com>
2024-04-05 13:09:25 +0800

    完善一下pushplus的消息返回，增加总点数的提醒

--- main.js
+++ main.js
@@ -13,24 +13,33 @@ const gladosRequestPush=async()=>{
       'referrer':'https://glados.rocks/console/checkin',//网络请求发出的那个地方的网址
       'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
     }
+
+    //签到请求的目的url
     const checkinRequestURL='https://glados.rocks/api/user/checkin'
+    //发送签到请求
     const checkin=await fetch(checkinRequestURL,{
       method:'POST',
       headers:{...headers,  'content-type':'application/json;charset=UTF-8'},
       //只有POST方法需要content-type
       body:'{"token":"glados.one"}'
     }).then((r)=>r.json())
+
+    //获取当前状态的请求的目的url
     const statusRequestURL ='https://glados.rocks/api/user/status'
+    //发送获取当前状态的请求
     const status=await fetch(statusRequestURL,{
       method:'GET',
       headers,
     }).then((r)=>r.json());
+    //返回值供pushplus使用
     return [
       'GLADOS 签到成功',
-      `剩余天数:${Number(status.data.leftDays)}`,
-      `签到提示:${checkin.message}`
+      `leftDays:${Number(status.data.leftDays)} `,
+      `签到消息:${checkin.message} `,
+      `总点数:${Number(checkin.list[0].balance)}`
     ]
   }catch(error){
+    //返回值供pushplus使用
     return [
       'GLADOS 签到失败',
       `失败信息${error}`,
@@ -40,8 +49,11 @@ const gladosRequestPush=async()=>{
 }
 
 const notify = async (contents) => {
+  //填写消息内容
   const [title , ...body]=contents
+  //pushplus给个人准备的标识
   const token = process.env.NOTIFY
+  //必要信息不完全，则不执行
   if (!token || !contents) return
   await fetch(`https://www.pushplus.plus/send`, {
     method: 'POST',
